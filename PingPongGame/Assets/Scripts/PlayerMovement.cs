e77dce1a7943f3d347bb5448d0b79178c0a94941
Uğur Evren Çamalan <ugurevrencamalan@gmail.com>
2023-01-14 19:42:34 +0300

    Finished Character Movement and Animations & Change platform to Android

--- PingPongGame/Assets/Scripts/PlayerMovement.cs
+++ PingPongGame/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,69 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    private Vector3 _direction;
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    public bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    private float gravitiyMultiplier = 20f;
     private Joystick _joystick;
-
+    private float _elapsedTime;
+    public float _touchSensivity = 0.5f;
+    private float moving;
+    Animator _animator;
+   
+    
     private void Start()
     {
+        controller = gameObject.GetComponent<CharacterController>();
         _joystick = Joystick.Instance;
+        _animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        _direction = _joystick.Direction;
-        if (Input.GetMouseButtonDown(0))
-        {
-            Jump();
-        }
-    }
-
-    void Jump()
-    {
+        groundedPlayer = controller.isGrounded;
         
+        moving = _joystick.Direction.magnitude;
+        _animator.SetFloat("moving",moving);
+        
+        
+        if (Input.GetMouseButton(0))
+        {
+            _elapsedTime += Time.deltaTime;
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            if (_elapsedTime < _touchSensivity && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight  * -3 * gravityValue);
+                _animator.SetTrigger("Jump");
+                
+            }
+
+            _elapsedTime = 0;
+        }
+        //groundedPlayer = true;
+        if (playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue *gravitiyMultiplier* Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
